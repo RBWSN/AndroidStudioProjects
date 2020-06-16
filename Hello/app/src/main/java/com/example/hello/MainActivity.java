4eef825d7863f53e9665a3ecf4ddc36df5f36ffd
RBWSN <kinu72002@naver.com>
2020-06-17 04:48:02 +0900

    check button radio

--- Hello/app/src/main/java/com/example/hello/MainActivity.java
+++ Hello/app/src/main/java/com/example/hello/MainActivity.java
@@ -14,9 +14,11 @@ import android.widget.ScrollView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    ScrollView scrollView;
+
     ImageView imageView;
-    BitmapDrawable bitmap;
+    ImageView imageView2;
+    int imageIndex = 0;
+
 
 
     @Override
@@ -24,54 +26,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        scrollView = (ScrollView) findViewById(R.id.scrollView);
-        imageView = (ImageView)findViewById(R.id.imageView); // 뷰객체 레이아웃 참조
-        scrollView.setHorizontalScrollBarEnabled(true); // 수평 스크롤바 사용 기능 설정
-
-        Resources res = getResources();
-        bitmap = (BitmapDrawable) res.getDrawable(R.drawable.a1231);
-        int bitmapWidth = bitmap.getIntrinsicWidth();
-        int bitmapHeight = bitmap.getIntrinsicHeight(); // 리소스 이미지 참조
-
-        imageView.setImageDrawable(bitmap);
-        imageView.getLayoutParams().width = bitmapWidth;
-        imageView.getLayoutParams().height = bitmapHeight; // 이미지 리소스와 크기 설정
-
-    }
-
-
-    public void onMenuButtonClicked(View view) {
-        changeImg();
-
-    }
-
-    public void changeImg() {
-
-
-        Resources res= getResources();
-
-
-        if(bitmap == (BitmapDrawable) res.getDrawable(R.drawable.a1233)){
-            bitmap = (BitmapDrawable) res.getDrawable(R.drawable.a1231);
-
-
-        }else{
-            bitmap = (BitmapDrawable) res.getDrawable(R.drawable.a1231);
-
-        }
-        bitmap = (BitmapDrawable) res.getDrawable(R.drawable.a1233);
-
-
-
-        int bitmapWidth = bitmap.getIntrinsicWidth();
-        int bitmapHeight = bitmap.getIntrinsicHeight();
-
-        imageView.setImageDrawable(bitmap);
-        imageView.getLayoutParams().width = bitmapWidth;
-        imageView.getLayoutParams().height = bitmapHeight;
-
-
 
 
     }
+
 }
