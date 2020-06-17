5a5e967d7f10bc40bdcf3b645e70e22cecc0d029
RBWSN <kinu72002@naver.com>
2020-06-18 01:19:04 +0900

    edittext TextWatcher and Listener

--- Hello/app/src/main/java/com/example/hello/MainActivity.java
+++ Hello/app/src/main/java/com/example/hello/MainActivity.java
@@ -2,25 +2,26 @@ package com.example.hello;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ScrollView;
+import android.widget.TextSwitcher;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    ScrollView scrollView1;
-    ScrollView scrollView2;
-    ImageView imageView1;
-    ImageView imageView2;
-    BitmapDrawable bitmap;
 
-
+EditText text;
 
 
 
@@ -29,63 +30,48 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        scrollView1 = (ScrollView) findViewById(R.id.scrollView1);
-        scrollView2 = (ScrollView) findViewById(R.id.scrollView2); // 수평 스크롤 뷰 사용
-
-        scrollView2.setHorizontalScrollBarEnabled(true);
 
 
+        text = (EditText) findViewById(R.id.textee); // 에디트 텍스트 연결
 
+        text.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
+            }
 
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                //에디터 텍스트가 변경되었을때 작동하는 리스너
 
-    }
+                int edittext = text.getText().length();
+                String to = Integer.toString(edittext);
 
-    public void onUp(View view) {
-        scrollView1 = (ScrollView) findViewById(R.id.scrollView1);
-        scrollView1.setHorizontalScrollBarEnabled(true);
-
-        imageView1 = (ImageView) findViewById(R.id.image01);
-        imageView2 = (ImageView) findViewById(R.id.image02);
-
-
-
-        Resources res = getResources();
-        bitmap = (BitmapDrawable) res.getDrawable(R.drawable.a1231);
-
-        int bitmapWidth = bitmap.getIntrinsicWidth();
-        int bitmapHeight = bitmap.getIntrinsicHeight();
-
-        imageView1.setImageDrawable(bitmap);
-        imageView1.getLayoutParams().width = bitmapWidth;
-        imageView1.getLayoutParams().height = bitmapHeight;
-
-        imageView2.setVisibility(View.INVISIBLE);
-        imageView1.setVisibility(View.VISIBLE);
-    }
-
-
-    public void onButtom(View view){
-        scrollView2 = (ScrollView) findViewById(R.id.scrollView2);
-        scrollView2.setHorizontalScrollBarEnabled(true);
-
-        imageView1 = (ImageView) findViewById(R.id.image01);
-        imageView2 = (ImageView) findViewById(R.id.image02);
+                TextView textView;
+                textView = findViewById(R.id.textv);
+                textView.setText(to+"  /  80");
 
 
 
-        Resources res = getResources();
-        bitmap = (BitmapDrawable) res.getDrawable(R.drawable.a1231);
+            }
 
-        int bitmapWidth = bitmap.getIntrinsicWidth();
-        int bitmapHeight = bitmap.getIntrinsicHeight();
+            @Override
+            public void afterTextChanged(Editable editable) {
 
-        imageView2.setImageDrawable(bitmap);
-        imageView2.getLayoutParams().width = bitmapWidth;
-        imageView2.getLayoutParams().height = bitmapHeight;
+            }
+        });
 
-        imageView1.setVisibility(View.INVISIBLE);
-        imageView2.setVisibility(View.VISIBLE);
+
+
 
     }
+
+   public void onSend(View view){
+       text = (EditText)findViewById(R.id.textee);
+        String messageall = text.getText().toString();
+
+        Toast.makeText(this.getApplicationContext(),messageall ,Toast.LENGTH_LONG).show();
+
+   }
+
 }
